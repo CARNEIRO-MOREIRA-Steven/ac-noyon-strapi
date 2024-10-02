8628619cc492eed0546ae4ee011871b88188c641
Squaad6z <carneiromoreirasteven22@gmail.com>
2024-10-02 18:12:51 +0200

    Seconde API

---
+++ src/api/dernierseevenement/controllers/dernierseevenement.ts
@@ -0,0 +1,7 @@
+/**
+ * dernierseevenement controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::dernierseevenement.dernierseevenement');
---
+++ src/api/dernierseevenement/routes/dernierseevenement.ts
@@ -0,0 +1,7 @@
+/**
+ * dernierseevenement router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::dernierseevenement.dernierseevenement');
